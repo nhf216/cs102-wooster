4eb3be93a36f949915d56ea549f37f4cc62921d3
= <=>
2019-08-14 22:28:22 -0400

    Updated media_configure to work with PyQt5

--- media_configure.py
+++ media_configure.py
@@ -4,16 +4,20 @@
 #as any files that depend on it.
 import sys
 import os
-import PyQt4.QtGui
-# Create an PyQT4 application object.
-#If we're running in Canopy, there already is one
-root = PyQt4.QtGui.QApplication.instance()
+try:
+    import PyQt5.QtWidgets as QtGui
+except ImportError:
+    import PyQt4.QtGui as QtGui
+# Create an PyQT4tg in Canopy, there already is one
+root = QtGui.QApplication.instance()
 if root is None:
     #We're not running in Canopy
     #Need to launch a new application
-    root = PyQt4.QtGui.QApplication(sys.argv)
-media = PyQt4.QtGui.QFileDialog.getOpenFileName(caption = "Select media.py", filter = "media.py")
+    root = QtGui.QApplication(sys.argv)
+media = QtGui.QFileDialog.getOpenFileName(caption = "Select media.py", filter = "media.py")
 if media != '':
+    if isinstance(media, tuple):
+        media = media[0]
     media_loc = media[:media.rfind(os.sep)+1]
     if media_loc not in sys.path:
         sys.path.append(media_loc)
